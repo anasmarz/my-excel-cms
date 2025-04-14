d167589c9f92c9f9ce5669fc69658cee5566f113
AnasUpm <muhammadanasmarz@gmail.com>
2025-04-14 22:50:35 +0800

    Update database.ts

--- config/database.ts
+++ config/database.ts
@@ -25,15 +25,10 @@ export default ({ env }) => {
     },
     postgres: {
       connection: {
-        ...(env('DATABASE_URL') ? parse(env('DATABASE_URL')) : {}), // Parse URL if exists
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) ? { 
-          rejectUnauthorized: false 
-        } : false,
+        ...parse(env('DATABASE_URL')), // Remove hardcoded URL from here
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false)
+        },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
